05c3245399dd94f548ea9aa36e5d5c4d2ff0d2fd
ycjx <897804105@qq.com>
2019-03-21 23:50:04 +0800

    学习一下Exchanger

--- src/java.base/share/classes/java/util/concurrent/Exchanger.java
+++ src/java.base/share/classes/java/util/concurrent/Exchanger.java
@@ -250,6 +250,8 @@ public class Exchanger<V> {
     /**
      * The index distance (as a shift value) between any two used slots
      * in the arena, spacing them out to avoid false sharing.
+     *
+     * 任何两个使用的槽之间的索引距离 ，将他们隔开避免错误共享
      */
     private static final int ASHIFT = 5;
 
@@ -288,6 +290,7 @@ public class Exchanger<V> {
      * Value representing null arguments/returns from public
      * methods. Needed because the API originally didn't disallow null
      * arguments, which it should have.
+     * 当传入null 用NULL_ITEM 代替
      */
     private static final Object NULL_ITEM = new Object();
 
@@ -303,12 +306,16 @@ public class Exchanger<V> {
      * bookkeeping. Padded via @Contended to reduce memory contention.
      */
     @jdk.internal.vm.annotation.Contended static final class Node {
+
+        //数组槽的索引值
         int index;              // Arena index
         int bound;              // Last recorded value of Exchanger.bound
         int collides;           // Number of CAS failures at current bound
         int hash;               // Pseudo-random for spins
+        //这个线程需要交换的数据
         Object item;            // This thread's current item
         volatile Object match;  // Item provided by releasing thread
+        //
         volatile Thread parked; // Set to this thread when parked, else null
     }
 
@@ -349,17 +356,36 @@ public class Exchanger<V> {
      * @param ns if timed, the maximum wait time, else 0L
      * @return the other thread's item; or null if interrupted; or
      * TIMED_OUT if timed and timed out
+     *
+     * 多槽交换的方法
+     * 多槽的交换大致思想就是：当一个线程来交换的时候，如果”第一个”槽位是空的，那么自己就在那里等待，
+     * 如果发现”第一个”槽位有等待线程，那么就直接交换，如果交换失败，说明其它线程在进行交换，
+     * 那么就往后挪一个槽位，如果有数据就交换，没数据就等一会，但是不会阻塞在这里，在这里等了一会，
+     * 发现还没有其它线程来交换数据，那么就往“第一个”槽位的方向挪，如果反复这样过后，挪到了第一个槽位，
+     * 没有线程来交换数据了，那么自己就在”第一个”槽位阻塞等待。简单来说，如果有竞争冲突，那么就寻找后面的槽位，
+     * 在后面的槽位等待一定时间，没有线程来交换， 那么就又往前挪。
+     * ---------------------
+     *
+     * 这段话是从这个博主那抄过来的
+     * 作者：zthgreat
+     * 来源：CSDN
+     * 原文：https://blog.csdn.net/u014634338/article/details/78385521
+     * 版权声明：本文为博主原创文章，转载请附上博文链接！
      */
     private final Object arenaExchange(Object item, boolean timed, long ns) {
         Node[] a = arena;
         int alen = a.length;
+        //当前线程的node p
         Node p = participant.get();
         for (int i = p.index;;) {                      // access slot at i
             int b, m, c;
+            //根据p.index算出所在数组的索引j
             int j = (i << ASHIFT) + ((1 << ASHIFT) - 1);
             if (j < 0 || j >= alen)
                 j = alen - 1;
+            // q为数组j索引所在节点
             Node q = (Node)AA.getAcquire(a, j);
+            //如果 q不为空就进行交换 交换成功那么就唤醒q所在线程
             if (q != null && AA.compareAndSet(a, j, q, null)) {
                 Object v = q.item;                     // release
                 q.match = item;
@@ -368,19 +394,28 @@ public class Exchanger<V> {
                     LockSupport.unpark(w);
                 return v;
             }
+            // bound 是最大的有效的 位置，和MMASK相与，得到真正的存储数据的索引最大值
+            // boud赋值给m ，m下面会用到
             else if (i <= (m = (b = bound) & MMASK) && q == null) {
+                //如果q为空并且i在范围之内
+                //将需要交换的数据交给p
                 p.item = item;                         // offer
+                //设置数组arean 索引j的值为p 等待其它线程来交换
                 if (AA.compareAndSet(a, j, null, p)) {
                     long end = (timed && m == 0) ? System.nanoTime() + ns : 0L;
                     Thread t = Thread.currentThread(); // wait
+                    //开始自选
                     for (int h = p.hash, spins = SPINS;;) {
                         Object v = p.match;
+                        //v != null 说明已经有线程来交换过了
                         if (v != null) {
+                            //交换数据 返回得到的数据 并还原相关设置
                             MATCH.setRelease(p, null);
                             p.item = null;             // clear for next use
                             p.hash = h;
                             return v;
                         }
+                        // match为空的话，就在这里进行一点的自炫
                         else if (spins > 0) {
                             h ^= h << 1; h ^= h >>> 3; h ^= h << 10; // xorshift
                             if (h == 0)                // initialize hash
@@ -389,8 +424,13 @@ public class Exchanger<V> {
                                      (--spins & ((SPINS >>> 1) - 1)) == 0)
                                 Thread.yield();        // two yields per wait
                         }
+                        // j索引的位置已经不是p了，但是match还是为空
+                        //说明有线程来交换了，但是mactch值还没有交换
+                        // 重置spins 再自旋一会
                         else if (AA.getAcquire(a, j) != p)
                             spins = SPINS;       // releaser hasn't set match yet
+
+                        //当m==0时 说明已经已经是第一个了，还是没有人来交换，那就正式阻塞自己
                         else if (!t.isInterrupted() && m == 0 &&
                                  (!timed ||
                                   (ns = end - System.nanoTime()) > 0L)) {
@@ -403,12 +443,15 @@ public class Exchanger<V> {
                             }
                             p.parked = null;
                         }
+                        //索引j还是p 那就将j位置设置为空
                         else if (AA.getAcquire(a, j) == p &&
                                  AA.compareAndSet(a, j, p, null)) {
+                            //更新bound
                             if (m != 0)                // try to shrink
                                 BOUND.compareAndSet(this, b, b + SEQ - 1);
                             p.item = null;
                             p.hash = h;
+                            //减小索引值 从最后的索引值慢慢的往第一个移动
                             i = p.index >>>= 1;        // descend
                             if (Thread.interrupted())
                                 return null;
@@ -419,20 +462,30 @@ public class Exchanger<V> {
                     }
                 }
                 else
+                    //占据失败
                     p.item = null;                     // clear offer
             }
+            // i不在范围内
             else {
-                if (p.bound != b) {                    // stale; reset
+
+                //bound 变了
+                if (p.bound != b) {
+                    // stale; reset
+                    // 更新b
                     p.bound = b;
                     p.collides = 0;
+                    //如果当前i在最大索引位置，那么递减一位
                     i = (i != m || m == 0) ? m : m - 1;
                 }
+                // 这里有点没有看懂
                 else if ((c = p.collides) < m || m == FULL ||
                          !BOUND.compareAndSet(this, b, b + SEQ + 1)) {
                     p.collides = c + 1;
+                    // i=0 那么就从m开始，否则递减i
                     i = (i == 0) ? m : i - 1;          // cyclically traverse
                 }
                 else
+                    //递增
                     i = m + 1;                         // grow
                 p.index = i;
             }
@@ -448,31 +501,72 @@ public class Exchanger<V> {
      * @return the other thread's item; or null if either the arena
      * was enabled or the thread was interrupted before completion; or
      * TIMED_OUT if timed and timed out
+     *
+     *
+     *
+     * 这个方法是当只有单槽位的时候进行数据交换
+     * 第一种情况  slot不为空 尝试cas替换slot为null并成功，唤醒阻塞的线程，
+     *           将要替换的item赋值给阻塞node.match 返回阻塞node.item
+     *           返回 要的值，当前slot为null
+     *
+     * 第二种情况  slot不为空 cas失败，bound为0，则将创建arena空数组，并进入下次自旋
+     *           如果下次进来发现slot被人抢走了，并且arena也不为空 则返回null
+     *           返回null arena不为null slot为 null
+     *
+     * slot如果存在，线程会一直抢直到抢到或者arena不为空，
+     *
+     *
+     * 第三种情况  当slot为空，并且arena也为空时 首先会尝试下占领slot如果成功就会break，
+     *           失败的话，说明slot被其他线程占领，又要进入第一第二种情况
+     *
+     * 接下来 如果当前线程占领了slot 再进行一定程度的自旋，
+     * （这里有个疑问 一个线程占领了slot并开始自旋，在自旋过程中，另外个进场进来进入第一种情况把
+     *   slot cas 为null 第一个线程岂不是一直自旋下去 ）
+     *  (q.match = item;  漏看了这么一行代码，坑爹)
+     *
+     *  再自旋过程中如果有另外线程又占领了slot，那么该线程的match则被复制为那个线程的item,并返回item
+     *
+     *  如果没有线程来抢走
+     *  arena为空则阻塞当前线程
+     *  arena不为空则将slot设置成null 并返回null 并将MACTCH设置成当前节点（这种情况怎么才会发生呢。。）
+     *
+     *
+     *
      */
     private final Object slotExchange(Object item, boolean timed, long ns) {
+        //获取当前线程的节点
         Node p = participant.get();
         Thread t = Thread.currentThread();
+        //线程中断立即返回
         if (t.isInterrupted()) // preserve interrupt status so caller can recheck
             return null;
 
+        //自旋
         for (Node q;;) {
+            // slot 不为null 说明已有线程将数据插入slot中并阻塞等待唤醒
             if ((q = slot) != null) {
+                //尝试cas替换
                 if (SLOT.compareAndSet(this, q, null)) {
+                    // 交换的数据
                     Object v = q.item;
                     q.match = item;
+                    //获取阻塞的线程并唤醒
                     Thread w = q.parked;
                     if (w != null)
                         LockSupport.unpark(w);
                     return v;
                 }
+                //cas失败 创建arena数组 bound为0说明数组还没有创建 cas bound 为256
                 // create arena on contention, but continue until slot null
                 if (NCPU > 1 && bound == 0 &&
                     BOUND.compareAndSet(this, 0, SEQ))
                     arena = new Node[(FULL + 2) << ASHIFT];
             }
+            //如果arena != null，直接返回，进入arenaExchange 多个槽位的交换
             else if (arena != null)
                 return null; // caller must reroute to arenaExchange
             else {
+                //如果slot为null 并且area也为null 则将当前线程的node占领slot
                 p.item = item;
                 if (SLOT.compareAndSet(this, null, p))
                     break;
@@ -482,10 +576,15 @@ public class Exchanger<V> {
 
         // await release
         int h = p.hash;
+        //默认end都为0L
         long end = timed ? System.nanoTime() + ns : 0L;
+
+        // 自旋次数 NCPU<=1 禁用自旋
         int spins = (NCPU > 1) ? SPINS : 1;
         Object v;
+        //进入自旋
         while ((v = p.match) == null) {
+            //进行
             if (spins > 0) {
                 h ^= h << 1; h ^= h >>> 3; h ^= h << 10;
                 if (h == 0)
@@ -493,11 +592,14 @@ public class Exchanger<V> {
                 else if (h < 0 && (--spins & ((SPINS >>> 1) - 1)) == 0)
                     Thread.yield();
             }
+            //自旋完成后如果 slot变了 重置spins重新自旋
             else if (slot != p)
                 spins = SPINS;
+            //如果线程没有被中断并且arena为空
             else if (!t.isInterrupted() && arena == null &&
                      (!timed || (ns = end - System.nanoTime()) > 0L)) {
                 p.parked = t;
+                //如果当前线程已经占领了slot并且时间到了阻塞当前线程
                 if (slot == p) {
                     if (ns == 0L)
                         LockSupport.park(this);
@@ -506,6 +608,7 @@ public class Exchanger<V> {
                 }
                 p.parked = null;
             }
+            //当线程中断或者 arena不为空的时候 p 是否占领slot 是的话为null
             else if (SLOT.compareAndSet(this, p, null)) {
                 v = timed && ns <= 0L && !t.isInterrupted() ? TIMED_OUT : null;
                 break;
@@ -562,6 +665,9 @@ public class Exchanger<V> {
         Object v;
         Node[] a;
         Object item = (x == null) ? NULL_ITEM : x; // translate null args
+
+        //当arena数组槽为空时，调用slotExchange方法
+        //当arena数组槽不为空时或者slotExchange返回null 调用arenaExchange
         if (((a = arena) != null ||
              (v = slotExchange(item, false, 0L)) == null) &&
             ((Thread.interrupted() || // disambiguates null return
